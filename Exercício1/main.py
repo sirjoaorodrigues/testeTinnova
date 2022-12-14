c1c1ba373172200f247507641cd0dd1fb22a1244
sirjoaorodrigues <jjrdrgs@gmail.com>
2022-12-14 19:17:04 -0300

    Exercício 1 Concluído

--- Exercício1/main.py
+++ Exercício1/main.py
@@ -6,6 +6,12 @@ class Vote:
         self.white_votes = white_votes
         self.null_votes = null_votes
 
+    def __repr__(self):
+        return (f'Votos totais: {self.total_votes} votos\n'
+                f'Votos válidos: {self.valid_votes} votos\n'
+                f'Votos em branco: {self.white_votes} votos\n'
+                f'Votos nulos: {self.null_votes} votos\n')
+
     def valid_percent(self):
         return f'Percentual de votos válidos: {int((self.valid_votes / self.total_votes) * 100)}%'
 
@@ -14,12 +20,6 @@ class Vote:
 
     def null_percent(self):
         return f'Percentual de votos nulos: {int((self.null_votes / self.total_votes) * 100)}%'
-
-    def __repr__(self):
-        return (f'Votos totais: {self.total_votes} votos\n'
-                f'Votos válidos: {self.valid_votes} votos\n'
-                f'Votos em branco: {self.white_votes} votos\n'
-                f'Votos nulos: {self.null_votes} votos\n')
 
 
 if __name__ == '__main__':
